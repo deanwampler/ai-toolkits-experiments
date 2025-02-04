a7ff601baf62721a641c0b7b49bfb89eee7f7aad
Dean Wampler <dean.wampler@ibm.com>
2025-02-04 12:32:39 -0500

    Updated model list and functionality
    Signed-off-by: Dean Wampler <dean.wampler@ibm.com>

--- ollama/models-install.sh
+++ ollama/models-install.sh
@@ -1,17 +1,26 @@
 models=(
 codellama
 codellama:13b
+codestral
+deepseek-r1:8b
+granite-code:3b
+granite-code:8b
+granite-code:20b
 starcoder2
 starcoder2:7b
 starcoder2:15b
-codestral
-granite-code:3b
-granite-code:8b
-granite-code:20b)
+)
 
 for model in ${models[@]}
 do
-  echo $model
-  ollama pull "$model"
+  printf '%s: ' $model
+  ollama list | grep -q $model
+  if [[ $? -eq 0 ]]
+  then
+    echo "already installed."
+  else
+    echo "installing..."
+    ollama pull "$model"
+  fi
 done
 beep
