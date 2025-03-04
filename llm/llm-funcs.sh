590a33cc52a93f2a0245edb634bfb58f8d482b93
Dean Wampler <dean.wampler@ibm.com>
2025-03-03 20:24:24 -0500

    Tweaked the llm helper functions.
    Signed-off-by: Dean Wampler <dean.wampler@ibm.com>

--- llm/llm-funcs.sh
+++ llm/llm-funcs.sh
@@ -1,22 +1,49 @@
 # This file defines two zsh functions for working with llm and Sublime Text
 # This file is meant to be sourced, e.g., `. llm-funcs.sh`.
-# The functions are adapted from 
+# Some of the functions are adapted from 
 # https://rtl.chrisadams.me.uk/2024/12/how-i-use-llms-neat-tricks-with-simons-llm-tool/
 # See also the README in this directory.
 
 # Change this definition for your preferred model.
-export LLM_MODEL=llama3.3
+: ${LLM_MODEL:=llama3.3}
+export LLM_MODEL
+
+llm-envs() {
+    env | egrep '^LLM_[_a-zA-Z0-9]*'
+}
 
 # Open a temp file in Sublime Text, output the contents when closed, then pipe
 # that output to llm.
 # NOTE: I tried using $EDITOR, but zsh seems to interpret "subl -w" as the command.
 # If you prefer VSCode, see the embedded comments about what to use instead of "subl -w".
-prompt-llm() {
+llm-prompt() {
+    model=$LLM_MODEL 
+    help_msg="llm-prompt [-h|--help] [-m|--model M]"
+    while [[ $# -gt 0 ]]
+    do
+        case $1 in
+            -h|--help)
+                echo $help_msg
+                return 0
+                ;;
+            -m|--model)
+                shift
+                model=$1
+                ;;
+            *)
+                echo "ERROR: $help_msg"
+                return 1
+                ;;
+        esac
+        shift
+    done
+    echo "using model: $model"
+
     # Create a temporary file
     tempfile=$(mktemp)
 
     # Open the editor on the file and wait for it to close.
-    # For VSCode, use "code --wait" instead of "subl -w.
+    # For VSCode, use "code --wait" instead of "subl -w".
     subl -w $tempfile
 
     # If the file has content, output it and then remove the file.
@@ -27,7 +54,7 @@ prompt-llm() {
     else
         rm $tempfile
         return 1
-    fi | llm --model $LLM_MODEL 
+    fi | llm --model $model 
 }
 
 # Pipe a question into llm and display the output in Sublime Text
